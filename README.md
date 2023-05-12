1160c46d829bde53262f8d00a89738a64a39b5d7
arunabhdas <arunabhdas@gmail.com>
2023-05-12 02:03:22 -0400

    Scaffold unicorn app using flutter

---
+++ README.md
@@ -0,0 +1,9 @@
+# UnicornApp 
+
+* UnicornApp for macOS (using Flutter)
+
+## Steps
+
+```
+flutter create unicornapp --org app.unicornapp
+```
---
+++ unicornapp/android/app/src/main/kotlin/app/unicornapp/unicornapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.unicornapp.unicornapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
